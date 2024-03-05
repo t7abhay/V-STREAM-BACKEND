c94b76727502298be590ba851679cbce061d4b28
ABHAY TARAS <abhaytaras18@gmail.com>
2024-03-05 13:13:12 +0530

    new

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,48 +1,47 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, {Schema} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-
-
-
 const videoSchema = new Schema(
-   {
-      videoFile: {
-         type: String,
-         required: true,
-      },
-      thumbnail: {
-         type: String,
-         required: true,
-      },
-      title: {
-         type: String,
-         required: true,
-      },
-      description: {
-         type: String,
-         required: true,
-      },
-      duration: {
-         type: Number,
-         required: true,
-      },
-      views: {
-         type: Number,
-         default: 0,
-      },
+    {
+        videoFile: {
+            type: String, //cloudinary url
+            required: true
+        },
+        thumbnail: {
+            type: String, //cloudinary url
+            required: true
+        },
+        title: {
+            type: String, 
+            required: true
+        },
+        description: {
+            type: String, 
+            required: true
+        },
+        duration: {
+            type: Number, 
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: true
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
 
-      isPublished: {
-         type: Boolean,
-         default: true,
-      },
-      owner: {
-         type: Schema.Types.ObjectId,
-         ref: "User",
-      },
-   },
-   { timestamps: true }
-);
+    }, 
+    {
+        timestamps: true
+    }
+)
 
 videoSchema.plugin(mongooseAggregatePaginate)
 
-export const Video = mongoose.model("VideoSchema", videoSchema);
+export const Video = mongoose.model("Video", videoSchema)
