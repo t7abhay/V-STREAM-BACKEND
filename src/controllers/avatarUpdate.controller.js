868e2660fcb230e8585bedae05f6f19b2bfc32e7
ABHAY TARAS <abhaytaras18@gmail.com>
2024-04-15 10:59:41 +0530

    Fix bug new avatar update add import ApiResponse fix avatarLocalPath extraction

--- src/controllers/avatarUpdate.controller.js
+++ src/controllers/avatarUpdate.controller.js
@@ -1,35 +1,33 @@
 import { asyncHandler } from "../utilities/asyncHandler.js";
 import { ApiError } from "../utilities/ApiError.js";
+import { ApiResponse } from "../utilities/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utilities/cloudinary.js";
- 
 
 const updateUserAvatar = asyncHandler(async (req, res) => {
-   const avatarLocalPath = req.files.path;
+    const avatarLocalPath = req.file?.path;
 
-   if (!avatarLocalPath) {
-      throw new ApiError(400, "Avatar file is missing");
-   }
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing");
+    }
 
-   const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
 
-   if (!avatar.url) {
-      throw new ApiError(400, "Error while uploading avatar");
-   }
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading avatar");
+    }
 
- const user =  await User.findByIdAndUpdate(
-      req.user?._id,
-      {
-         $set: {
-            avatar: avatar.url,
-         },
-      },
-      { new: true }.select("-password")
-   );
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url,
+            },
+        },
+        { new: true }
+    ).select("-password");
 
-   return res
-   .status(200)
-   .json(new ApiResponse(200,user,"Avatar updated"))
+    return res.status(200).json(new ApiResponse(200, user, "Avatar updated"));
 });
 
 export { updateUserAvatar };
