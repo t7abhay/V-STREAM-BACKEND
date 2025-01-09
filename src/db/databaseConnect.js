51bd21ae87c527404224a21694f2a02d9a32c8f4
t7abhay <abhaytaras18@gmail.com>
2025-01-09 14:14:09 +0530

    fix path

--- src/db/databaseConnect.js
+++ src/db/databaseConnect.js
@@ -7,10 +7,10 @@ const connectDB = async () => {
             `${process.env.MONGODB_URI}/${DB_NAME}`
         );
         console.log(
-            ` MongoDB connected !! DB Host: ${connectionInstance.connection.host}`
+            `\n MongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`
         );
     } catch (error) {
-        console.error("Mongo connection failed: ", error);
+        console.log("MONGODB connection FAILED: ", error);
         process.exit(1);
     }
 };
