daae3038e002755e8c4deca71756628a693f15b8
t7abhay <abhaytaras18@gmail.com>
2025-01-14 14:57:26 +0530

    handle cors globally

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ app.use(
         credentials: true,
     })
 );
+app.options("*", cors());
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
