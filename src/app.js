4a087a7909fd97d42c5f35f47b592511c502d10e
t7abhay <abhaytaras18@gmail.com>
2025-01-14 14:55:43 +0530

    .env change

--- src/app.js
+++ src/app.js
@@ -4,28 +4,20 @@ import cookieParser from "cookie-parser";
 import morgan from "morgan";
 
 const app = new express();
-
 app.use(
     cors({
         origin: process.env.CORS_ORIGIN,
         credentials: true,
     })
 );
-app.options(
-    "*",
-    cors({
-        origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-        credentials: true,
-    })
-);
 
-app.use(express.json({ limit: "500mb" }));
-app.use(express.urlencoded({ extended: true, limit: "500mb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 app.use(morgan("dev")); //HTTP request logger middleware for node.js
 
-
+//routes import
 
 import userRouter from "./routes/user.routes.js";
 import commentRouter from "./routes/comment.routes.js";
@@ -38,14 +30,14 @@ import playlistRouter from "./routes/playlist.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 
 //routes declaration
-app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/tweets", tweetRouter);
-app.use("/api/v1/subscriptions", subscriptionRouter);
-app.use("/api/v1/videos", videoRouter);
-app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/comment", commentRouter);
 app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/tweet", tweetRouter);
+app.use("/api/v1/video", videoRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 
-export { app };
+export default app;
