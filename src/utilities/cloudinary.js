7e9ab3f4e834056cc87ea508ce7cd88b37ac05e7
t7abhay <abhaytaras18@gmail.com>
2025-02-01 15:03:23 +0530

    added image moderation AI

--- src/utilities/cloudinary.js
+++ src/utilities/cloudinary.js
@@ -12,14 +12,13 @@ const uploadOnCloudinary = async (localFilePath) => {
     try {
         if (!localFilePath) return null;
 
-        //upload to cloudinary if localFilePath exists
         const result = await cloudinary.uploader.upload(localFilePath, {
             resource_type: 'auto',
+            moderation: "webpurify"
+
         });
 
-        // console.log("file uploaded to cloudinary", result.url);
-
-        fs.unlinkSync(localFilePath); //remove file from localFilePath after uploading to cloudinary
+        fs.unlinkSync(localFilePath);
         return result;
     } catch (error) {
         fs.unlinkSync(localFilePath);
@@ -31,13 +30,13 @@ const deleteOnCloudinary = async (public_id, resource_type = "image") => {
     try {
         if (!public_id) return null;
 
-        //delete file from cloudinary
+
         const result = await cloudinary.uploader.destroy(public_id, {
             resource_type: `${resource_type}`
         });
     } catch (error) {
         return error;
-        console.log("delete on cloudinary failed", error);
+
     }
 };
 
