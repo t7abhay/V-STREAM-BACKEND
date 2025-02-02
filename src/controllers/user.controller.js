8fe840f270ed34e6187f5abbab81243c323cb13c
t7abhay <abhaytaras18@gmail.com>
2025-02-02 20:32:07 +0530

    bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -251,9 +251,9 @@ const changeCurrentPassword = asyncHandler(async (req, res) => {
 
     const user = await User.findById(req.user?._id);
 
-    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
-    if (!isPasswordCorrect) {
-        throw new ApiError(400, "Invalid old password");
+    const isValidPassword = await user.isPasswordCorrect(oldPassword);
+    if (!isValidPassword) {
+        throw new ApiError(400, "Incorrect old password");
     }
 
     if (!(newPassword === confirmPassword)) {
