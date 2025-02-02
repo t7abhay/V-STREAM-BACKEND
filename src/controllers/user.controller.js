d3d8f808c71b179cb4b6efb050bc540ba0c73e78
t7abhay <abhaytaras18@gmail.com>
2025-02-02 18:26:14 +0530

    fix typo

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -144,7 +144,7 @@ const loginUser = asyncHandler(async (req, res) => {
         throw new ApiError(401, "Invalid user credentials.");
     }
 
-    const { accessToken, refreshToken } = await generateAccessAndRefreshToken(user._id);
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
 
     const loggedInUser = await User.findById(user._id).select(" -password -refreshToken");
 
