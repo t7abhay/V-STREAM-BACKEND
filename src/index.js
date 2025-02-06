9cb12ee704595f492185d2c0050317693fef66e1
ABHAY TARAS <abhaytaras18@gmail.com>
2025-02-06 16:57:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config({
 
 connectDB()
     .then(() => {
-        app.listen(process.env.GCP_PORT || 8000, () => {
+        app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is listening on: ${process.env.PORT}`);
         });
     })
