629b81d0362c002ccbc5db0586c6481d5079d4a4
t7abhay <abhaytaras18@gmail.com>
2025-04-09 17:09:46 +0530

    add ping chron job

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./db/databaseConnect.js";
 import app from "./app.js";
+import {servicePinger} from "./service.pinger.js";
 
 dotenv.config({
     path: "./.env",
@@ -10,7 +11,7 @@ connectDB()
     .then(() => {
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is listening on: ${process.env.PORT}`);
+            servicePinger();
         });
-
     })
     .catch((error) => console.log("MONGODB connection failed!!!: ", error));
---
+++ src/service.pinger.js
@@ -0,0 +1,19 @@
+import schedule from "node-schedule";
+import axios from "axios";
+
+export const servicePinger = () => {
+    schedule.scheduleJob("*/14 * * * *", async () => {
+        try {
+            const res = await axios.get(
+                "https://v-stream-backend.onrender.com/api/v1/healthcheck/health"
+            );
+            if (res.status === 200) {
+                console.log("🔁 Ping successful");
+            } else {
+                console.log(`⚠️ Ping responded with status: ${res.status}`);
+            }
+        } catch (err) {
+            console.error("❌ Ping failed:", err.message);
+        }
+    });
+};
